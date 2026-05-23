ee7629b19f797b12f883c570eb26b75f70311270
ssXue <529576446@qq.com>
2026-05-23 19:33:12 +0800

    feat: 新增 soem-sys FFI crate 和 SOEM C 源码
    - vendor/soem-src/: SOEM Git 源码（删除内嵌 .git）
    - vendor/soem-sys/: sys crate（build.rs 平台检测 + cc + bindgen）
    - macOS OSAL 重写（nanosleep 替代 clock_nanosleep）
    - 安全封装：safe_ecx_init/close/config_init/config_map_group/configdc/
      statecheck/readstate/send_processdata/receive_processdata/writestate
    - ContextPtr（Send 包装）+ cyclic_loop（后台线程，Result 返回）
    - zero_context + read_slave_name + read_slave_inputs + write_slave_outputs
    
    Signed-off-by: ssXue <529576446@qq.com>

---
+++ vendor/soem-src/contrib/osal/macosx/osal_defs.h
@@ -0,0 +1,48 @@
+/*
+ * This software is dual-licensed under GPLv3 and a commercial
+ * license. See the file LICENSE.md distributed with this software for
+ * full license information.
+ */
+
+#ifndef _osal_defs_
+#define _osal_defs_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// define if debug printf is needed
+#ifdef EC_DEBUG
+#define EC_PRINT printf
+#else
+#define EC_PRINT(...) \
+   do                 \
+   {                  \
+   } while (0)
+#endif
+
+#ifndef OSAL_PACKED
+#define OSAL_PACKED_BEGIN
+#define OSAL_PACKED __attribute__((__packed__))
+#define OSAL_PACKED_END
+#endif
+
+#include <pthread.h>
+#include <time.h>
+#define OSAL_THREAD_HANDLE  pthread_t *
+#define OSAL_THREAD_FUNC    void
+#define OSAL_THREAD_FUNC_RT void
+
+#ifndef ec_timet
+#define ec_timet struct timespec
+#endif
+
+#ifndef osal_mutext
+#define osal_mutext pthread_mutex_t
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ vendor/soem-sys/wrapper.h
@@ -0,0 +1,1 @@
+#include "soem/soem.h"
